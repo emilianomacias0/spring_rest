002aa67a5e938d2ff1e1b48958630e5dcecf76c1
regi2715 <emilianomacias0@gmail.com>
2019-10-28 23:21:32 -0600

    Informacion dummy

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO clientes (nombre,apellido,email,created_at) VALUES('Emiliano','Macias','emilianomacias0@gmail.com','2019-10-28');
+INSERT INTO clientes (nombre,apellido,email,created_at) VALUES('Daniel','Macias','emilianomacias0@gmail.com','2019-10-28');
+INSERT INTO clientes (nombre,apellido,email,created_at) VALUES('Jessica','Macias','emilianomacias0@gmail.com','2019-10-28');
+INSERT INTO clientes (nombre,apellido,email,created_at) VALUES('Silvia','Miranda','emilianomacias0@gmail.com','2019-10-28');
